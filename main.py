a90857f30eb3aa2bad515610be538b3b1e5535a8
shubham bajaj <shmbajaj32@gmail.com>
2024-06-13 13:14:15 +0530

    chore: api endpoints changed

--- main.py
+++ main.py
@@ -21,71 +21,22 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 @app.get("/")
-async def hello_world(request: Request):
-     await fetch_available_slots(logger=logger, selected_slot=None)
+async def hello_world():
+     return "Hello World"
+
+
+@app.post("/webhook")
+async def hello_webhook(request: Request):
+    try:
+            request_body = await request.json()
+            logger.info(
+                f"Received POST request: method={request.method}, path={request.url.path}, "
+                f"body={request_body}"
+            )
+    except:
+          pass
 
 @app.post("/")
-async def process_request(request: Request):
-    """
-    Handles POST requests to the root route.
-    Logs the request details and calls the create_chat_completion function.
-    """
-    request_body = await request.json()
-
-    logger.info(
-        f"Received POST request: method={request.method}, path={request.url.path}, "
-        f"body={request_body}"
-    )
-    tool_calls = request_body["message"]["toolCalls"]
-    # user_selected_slot = request_body.get("userSelectedSlot")
-    # mobileNumber = request_body.get("mobileNumber")
-
-    # Ensure 'message' and 'toolCalls' exist
-    if not tool_calls:
-        return JSONResponse(content={"error": "Invalid request format: missing 'message' or 'toolCalls'"}, status_code=400)
-    tool_call = tool_calls[0]
-
-    # Extract data from the tool call
-    tool_call_id = tool_call.get("id")
-    function_arguments = tool_call.get("function", {}).get("arguments", {})  # Safely access arguments
-    mobile_number = function_arguments.get("mobileNumber")
-    user_selected_slot = function_arguments.get("selectedSlot")
-    full_name = function_arguments.get("fullName")
-
-    # Log extracted data
-    logger.info(f"Extracted data: toolCallId={tool_call_id}, mobileNumber={mobile_number}, selectedSlot={user_selected_slot}")
-
-    if not user_selected_slot:
-        return JSONResponse(
-            content={"error": "Missing 'user_selected_slot' in request body"}, 
-            status_code=400
-        )
-
-    try:
-        selected_slot = await create_chat_completion(user_selected_slot, logger)
-        logger.info(f"Received response from OpenAI API: {selected_slot}")
-        scheduled_appointment_response = await create_appointment(mobile_number, full_name, selected_slot, logger)
-        scheduled_appointment_response_message = scheduled_appointment_response["message"]
-        formatted_response = {
-        "results": [
-            {
-                "toolCallId": tool_call_id,
-                "result": scheduled_appointment_response_message,
-            }
-        ]
-        }
-        return JSONResponse(
-            content=formatted_response, 
-            status_code=200
-        )
-    except Exception as e:
-        logger.critical(f"An unexpected error occurred: {e}")
-        return JSONResponse(
-            content={"error": "Internal Server Error"}, 
-            status_code=500
-        )
-
-@app.post("/book")
 async def fetchSlotsAndBookAppointment(request: Request):
     try:
         request_body = await request.json()
