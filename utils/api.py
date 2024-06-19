4821330de535840e5732dea56a7eeb4e96fcfb02
shubham bajaj <shmbajaj32@gmail.com>
2024-06-19 08:22:59 +0530

    feat: parse dtm str and get slots

--- utils/api.py
+++ utils/api.py
@@ -7,6 +7,31 @@ from ghl_cls import GoHighLevelClient
 from utils.ghl import datetime_str_to_epoch_ms, get_first_slots, get_next_business_day
 
 
+def parse_datetime_string(datetime_str):
+    """Parses a datetime string with ISO 8601 format and returns a datetime object."""
+
+    try:
+        # Use ISO 8601 format for parsing
+        parsed_datetime = datetime.fromisoformat(datetime_str)
+        now_edt = datetime.now(pytz.timezone("America/New_York"))
+        # Localize the parsed datetime to EDT (if it doesn't have a timezone)
+        if parsed_datetime.tzinfo is None:
+            parsed_datetime = pytz.timezone("America/New_York").localize(
+                parsed_datetime
+            )
+
+        # Compare in EDT timezone
+        # INFO: dependant on internal python comparison, didn't checked their algos
+        if parsed_datetime < now_edt:
+            return now_edt
+        else:
+            return parsed_datetime
+    except ValueError:
+        raise ValueError(
+            "Invalid datetime string format. Use ISO 8601 format (YYYY-MM-DDTHH:MM:SS[-|+]ZZ)."
+        )
+
+
 # Extract request data and validation logic
 async def process_request(request: Request):
     """
@@ -35,12 +60,15 @@ async def process_request(request: Request):
 
 
 # Handle slot fetching and processing
-async def fetch_and_process_slots(current_datetime_edt, edt_timezone):
+async def fetch_and_process_slots(current_datetime_edt, edt_timezone, days=1):
     start_date = get_next_business_day(current_datetime_edt, is_start_date=True)
     next_day_end_time = edt_timezone.localize(
-        datetime.combine(start_date.date() + timedelta(days=1), time(17, 0))
+        datetime.combine(start_date.date() + timedelta(days=days), time(17, 0))
     )
     end_date = get_next_business_day(next_day_end_time, is_start_date=False)
+    # print(
+    #     f"start_date: {start_date} next_day_end_time:{next_day_end_time} end_date: {end_date}"
+    # )
     formatted_start_date = (
         start_date.strftime("%Y-%m-%d %H:%M:%S")
         + start_date.strftime("%z")[:3]
@@ -53,9 +81,12 @@ async def fetch_and_process_slots(current_datetime_edt, edt_timezone):
         + ":"
         + end_date.strftime("%z")[3:]
     )
+    # print(
+    #     f"formatted_start_date: {formatted_start_date} formatted_end_date: {formatted_end_date}"
+    # )
     start_epoch_ms = datetime_str_to_epoch_ms(formatted_start_date)
     end_epoch_ms = datetime_str_to_epoch_ms(formatted_end_date)
-
+    # print(f"start_epoch_ms {start_epoch_ms} end_epoch_ms  {end_epoch_ms}")
     ghl = GoHighLevelClient()
     result, status_code = await ghl.get_appointment_slots(start_epoch_ms, end_epoch_ms)
 
@@ -63,8 +94,13 @@ async def fetch_and_process_slots(current_datetime_edt, edt_timezone):
         raise HTTPException(
             status_code=status_code, detail=result
         )  # Use more specific exceptions
-
+    # print(f"result: {result}")
     slots = get_first_slots(result, formatted_start_date)
+    if len(slots) < 2:
+        return await fetch_and_process_slots(
+            current_datetime_edt=current_datetime_edt, edt_timezone=edt_timezone, days=2
+        )
+    # print(f"slots: {slots}")
     picked_slots = [item[1] for item in slots]
     return picked_slots
 
